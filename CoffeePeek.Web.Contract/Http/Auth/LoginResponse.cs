7df6042681cccbbc53e36a8150b609c83ccdc4c9
Arseny Stefanenko <stefisen@yandex.ru>
2025-11-12 16:42:27 +0300

    +web version

---
+++ CoffeePeek.Web.Contract/Http/Auth/LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CoffeePeek.Web.Contract.Http.Auth;
+
+public class LoginResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+}
