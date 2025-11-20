c9348c138463a45246ad9763b89b60ccca726cbf
Arseny Stefanenko <stefisen@yandex.ru>
2025-11-20 11:13:36 +0300

    Add ReviewCoffeeController and request/response handlers for coffee shop reviews

---
+++ CoffeePeek.BusinessLogic/RequestHandlers/CoffeeShop/GetAllReviewsRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using CoffeePeek.Contract.Dtos.CoffeeShop;
+using CoffeePeek.Contract.Requests.CoffeeShop;
+using CoffeePeek.Contract.Response;
+using CoffeePeek.Contract.Response.CoffeeShop;
+using CoffeePeek.Domain.Databases;
+using MapsterMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeePeek.BusinessLogic.RequestHandlers.CoffeeShop;
+
+public class GetAllReviewsRequestHandler(CoffeePeekDbContext dbContext, IMapper mapper) 
+    : IRequestHandler<GetAllReviewsRequest, Response<GetAllReviewsResponse>>
+{
+    public async Task<Response<GetAllReviewsResponse>> Handle(GetAllReviewsRequest request, CancellationToken cancellationToken)
+    {
+        var userReviews = await dbContext.Reviews
+            .AsNoTracking()
+            .Include(r => r.Shop)
+            .Where(r => r.UserId == request.UserId)
+            .OrderByDescending(r => r.CreatedAt)
+            .ToListAsync(cancellationToken);
+
+        var reviewDtos = mapper.Map<CoffeeShopReviewDto[]>(userReviews);
+
+        var response = new GetAllReviewsResponse(reviewDtos);
+
+        return Response.SuccessResponse<Response<GetAllReviewsResponse>>(response);
+    }
+}
---
+++ CoffeePeek.Contract/Response/CoffeeShop/GetAllReviewsResponse.cs
@@ -0,0 +1,8 @@
+﻿using CoffeePeek.Contract.Dtos.CoffeeShop;
+
+namespace CoffeePeek.Contract.Response.CoffeeShop;
+
+public class GetAllReviewsResponse(ICollection<CoffeeShopReviewDto> reviews)
+{
+    public ICollection<CoffeeShopReviewDto> Reviews { get; } = reviews;
+}
