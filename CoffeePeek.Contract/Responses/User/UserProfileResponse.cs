15a01fb56ed5941808770c3ccb1d3ad4d6dda07d
winst <goloologo@gmail.com>
2026-01-11 18:11:55 +0000

    *fix

--- CoffeePeek.Contract/Responses/User/UserProfileResponse.cs
+++ CoffeePeek.Contract/Responses/User/UserProfileResponse.cs
@@ -7,11 +7,9 @@ public class UserProfileResponse
     [Required] public string UserName { get; set; }
     public DateTime CreatedAtUtc { get; set; }
 
-    public string Token { get; set; }
-
     public string? About { get; set; }
     public string? AvatarUrl { get; set; }
-    public int? ReviewCount { get; set; } = 0;
-    public int? CheckInCount { get; set; } = 0;
-    public int? AddedShopsCount { get; set; } = 0;
+    public int ReviewCount { get; set; } = 0;
+    public int CheckInCount { get; set; } = 0;
+    public int AddedShopsCount { get; set; } = 0;
 }
