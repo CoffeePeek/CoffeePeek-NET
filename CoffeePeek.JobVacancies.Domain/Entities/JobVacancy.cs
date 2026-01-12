f9f98f39f1ca729a5ffe1449a1fadde9c758874e
Arseny Stefanenko <stefisen@yandex.ru>
2026-01-12 10:05:29 +0300

    *jobs

--- CoffeePeek.JobVacancies.Domain/Entities/JobVacancy.cs
+++ CoffeePeek.JobVacancies.Domain/Entities/JobVacancy.cs
@@ -7,22 +7,16 @@ public class JobVacancy
     public Guid Id { get; set; }
 
     // External source identifiers/metadata
-    [MaxLength(55)]
     public string ExternalId { get; set; } = null!;
-    [MaxLength(100)]
     public string Source { get; set; } = "hh.ru";
 
     // Core vacancy info
-    [MaxLength(100)]
     public string Title { get; set; } = null!;
-    [MaxLength(50)]
+
     public string Company { get; set; } = null!;
-    [MaxLength(255)]
     public string Url { get; set; } = null!;
     public CPJobType Type { get; set; }
-    [MaxLength(55)]
     public string? ProfessionalRole { get; set; }
-    [MaxLength(50)]
     public string? Area { get; set; }
     public Guid CityMapId { get; set; }
     public Guid CityId{ get; set; }
