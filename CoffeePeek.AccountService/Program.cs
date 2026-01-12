9ac281724fe52b94d99321c2549dcf33a3d9a2be
Arseny Stefanenko <stefisen@yandex.ru>
2026-01-12 16:14:25 +0300

    +sentry

--- CoffeePeek.AccountService/Program.cs
+++ CoffeePeek.AccountService/Program.cs
@@ -32,16 +32,7 @@ using OutboxEvent = CoffeePeek.Account.Domain.Events.OutboxEvent;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseSentry(options =>
-{
-    options.Dsn = builder.Configuration["Sentry:Dsn"];
-    options.SendDefaultPii = true;
-    options.SetBeforeSend((@event, _) =>
-    {
-        @event.ServerName = null;
-        return @event;
-    });
-});
+builder.WebHost.UseSentry();
 
 // Add tunneling middleware.
 // Note that UseSentryTunneling also needs to be called on the IApplicationBuilder
@@ -165,12 +156,8 @@ builder.Services.AddOutboxEventPublisher<OutboxEvent, AccountDbContext>();
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
-
-CreateHostBuilder(args);
     
 var app = builder.Build();
-
-app.UseSentryTunneling();
 
 // Middleware pipeline
 app.UseExceptionHandler();
@@ -187,18 +174,3 @@ app.MapControllers();
 
 SentrySdk.CaptureMessage("Hello Sentry");
 app.Run();
-
-
-return;
-
-static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            // Add the following line:
-            webBuilder.UseSentry(o =>
-            {
-                o.Dsn = "https://b2bda147d60802d281928d9080e91409@o4510357410611200.ingest.de.sentry.io/4510675300646992";
-                o.Debug = true;
-            });
-        });
