24960a4b7c994e1847e90d6e9d5687cde79fcd99
Arseny Stefanenko <goloologo@gmail.com>
2026-02-20 15:35:11 +0300

    *fix

---
+++ CoffeePeek.Shops.Application/Features/Review/CanCreateCoffeeShopReview/CanCreateCoffeeShopReviewResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace CoffeePeek.Shops.Application.Features.Review.CanCreateCoffeeShopReview;
+
+/// <summary>
+/// Represents the response indicating whether a coffee shop review can be created or edited.
+/// </summary>
+public record CanCreateCoffeeShopReviewResponse(bool CanCreate, Guid? ReviewId = null);
